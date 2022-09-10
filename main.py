b656aa7edf4843e37bd6d592190f7c6783c5b9be
Luis Ramos <feram99ag@gmail.com>
2022-09-10 11:49:23 -0400

    feat(log): add logging to file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+import sys
+from logging.handlers import RotatingFileHandler
 
 from PIL import Image, ImageDraw
 from rgbmatrix import RGBMatrix
@@ -12,6 +14,21 @@ def main():
 
 
 if __name__ == '__main__':
+    if '--debug' in sys.argv:
+        LOG_LEVEL = logging.DEBUG
+        sys.argv.remove('--debug')
+    else:
+        LOG_LEVEL = logging.INFO
+
+    logger = logging.getLogger('')
+    logger.setLevel(LOG_LEVEL)
+    handler = RotatingFileHandler(filename='now-playing.log',
+                                  maxBytes=5 * 1024 * 1024,  # 5MB
+                                  backupCount=4)
+    handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s: %(message)s',
+                                           datefmt='%m/%d/%Y %I:%M:%S %p'))
+    logger.addHandler(handler)
+
     matrix = RGBMatrix(options=led_matrix_options(args()))
     canvas = Image.new('RGB', (matrix.width, matrix.height))
     draw = ImageDraw.Draw(canvas)
